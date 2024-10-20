bdcb0127a4d6b05dc11c8b3442c1a8f3bfb66893
Ana Konjetić <anakonjetic@gmail.com>
2024-10-20 10:50:18 +0200

    Add project files.

---
+++ Repository/WayFarerDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WayFarer.Repository
+{
+    public class WayFarerDbContext : DbContext
+    {
+        public WayFarerDbContext(DbContextOptions<WayFarerDbContext> options) : base(options) { }
+
+
+        
+    }
+}
