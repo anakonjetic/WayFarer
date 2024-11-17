9b61c3d7a1483e28edbd83f6dd99a5aad2ced0de
Lucija Kranjcec <lucija.kranjcec@rise-world.com>
2024-11-17 22:47:37 +0100

    implement city review functionality

---
+++ Migrations/20241117212702_UpdateReviewToAllowNullableCityAndAttraction.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WayFarer.Migrations
+{
+    public partial class UpdateReviewToAllowNullableCityAndAttraction : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "CityId",
+                table: "Review",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "AttractionId",
+                table: "Review",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.UpdateData(
+                table: "Itinerary",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "EndDate", "StartDate" },
+                values: new object[] { new DateTime(2024, 11, 17, 22, 27, 1, 699, DateTimeKind.Local).AddTicks(8679), new DateTime(2024, 11, 17, 22, 27, 1, 699, DateTimeKind.Local).AddTicks(8676) });
+
+            migrationBuilder.UpdateData(
+                table: "User",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "DateOfBirth",
+                value: new DateTime(2024, 11, 17, 22, 27, 1, 699, DateTimeKind.Local).AddTicks(8447));
+
+            migrationBuilder.UpdateData(
+                table: "User",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "DateOfBirth",
+                value: new DateTime(2024, 11, 17, 22, 27, 1, 699, DateTimeKind.Local).AddTicks(8578));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "CityId",
+                table: "Review",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "AttractionId",
+                table: "Review",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Itinerary",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "EndDate", "StartDate" },
+                values: new object[] { new DateTime(2024, 11, 17, 16, 39, 39, 285, DateTimeKind.Local).AddTicks(8510), new DateTime(2024, 11, 17, 16, 39, 39, 285, DateTimeKind.Local).AddTicks(8508) });
+
+            migrationBuilder.UpdateData(
+                table: "User",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "DateOfBirth",
+                value: new DateTime(2024, 11, 17, 16, 39, 39, 285, DateTimeKind.Local).AddTicks(8389));
+
+            migrationBuilder.UpdateData(
+                table: "User",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "DateOfBirth",
+                value: new DateTime(2024, 11, 17, 16, 39, 39, 285, DateTimeKind.Local).AddTicks(8444));
+        }
+    }
+}
